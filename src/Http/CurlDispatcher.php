3d9907c7f21060f6986b302c86d530b80aa1d787
Victor <caoruister@gmail.com>
2020-04-20 18:54:59 +0800

    custom user agent as mobile

--- src/Http/CurlDispatcher.php
+++ src/Http/CurlDispatcher.php
@@ -34,7 +34,7 @@ class CurlDispatcher implements DispatcherInterface
         CURLOPT_ENCODING => '',
         CURLOPT_AUTOREFERER => true,
         CURLOPT_FOLLOWLOCATION => true,
-        CURLOPT_USERAGENT => 'Embed PHP library',
+        CURLOPT_USERAGENT => 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/83.0.4103.14 Mobile/15E148 Safari/604.1',
         CURLOPT_IPRESOLVE => CURL_IPRESOLVE_V4,
     ];
 
